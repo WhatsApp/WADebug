c1d99da9645adccae732a7d8da81c508aa03fa62
Peter Sun <sunpeter@fb.com>
2018-12-21 05:17:36 -0800

    Split get_log method in log_utils into separate methods - Refactor and Added unit tests
    Summary:
    Follow up diff. Based on comments made on this diff: https://our.intern.facebook.com/intern/diff/D12920351/
    
    Splitting out log_utils.get_logs into seperate functions, each step in get_logs is split into its own function. Each function takes in a wa_container object and will try to grab the log it is written to grab. All new functions will return a string which is the file location of the log that is saved on the users local filesystem. For some log types ( i.e. coredumps ) they are specific to a certain type of container, so they will return a None value if the container is not of the right type.
    
    Also added unit tests for each of these new functions that were added.
    
    Reviewed By: mengyiyuan
    
    Differential Revision: D13526016
    
    fbshipit-source-id: 7921db60

--- wadebug/wa_actions/log_utils.py
+++ wadebug/wa_actions/log_utils.py
@@ -46,38 +46,21 @@ def get_logs():
     log_files = []
     errors = []
     for wa_container in wa_containers:
-        container = wa_container.container
-        container_type = wa_container.container_type
         try:
-            container_logs = docker_utils.get_container_logs(container)
-            log_filename = os.path.join(OUTPUT_FOLDER, '{}-container.log'.format(container.name))
-            docker_utils.write_to_file_in_binary(log_filename, container_logs)
-            log_files.append(log_filename)
-
-            inspect_result = docker_utils.get_inspect_result(container)
-            inspect_result_filename = os.path.join(OUTPUT_FOLDER, '{}-inspect.log'.format(container.name))
-            docker_utils.write_to_file(
-                inspect_result_filename,
-                json.dumps(inspect_result, indent=1),
-            )
-            log_files.append(inspect_result_filename)
-            if docker_utils.WA_COREAPP_CONTAINER_TAG == container_type:
-                core_dump_results = docker_utils.get_core_dump_logs(container)
-                core_dump_filename = os.path.join(OUTPUT_FOLDER, '{}-coredump.log'.format(container.name))
-                docker_utils.write_to_file(
-                    core_dump_filename,
-                    core_dump_results,
-                )
+            container_log_filename = get_container_logs(wa_container)
+            log_files.append(container_log_filename)
+            inspect_log_filename = get_container_inspect_logs(wa_container)
+            log_files.append(inspect_log_filename)
+            core_dump_filename = get_corecontainer_coredumps_logs(wa_container)
+            if core_dump_filename is not None:
                 log_files.append(core_dump_filename)
-            if docker_utils.WA_WEBAPP_CONTAINER_TAG == container_type:
-                webapp_log = copy_additional_logs_for_webcontainer(container, WEB_LOG_PATH, WEB_LOG_FILE)
-                webapp_error_log = \
-                    copy_additional_logs_for_webcontainer(container, WEB_ERROR_LOG_PATH, WEB_ERROR_LOG_PATH)
+            webapp_log, webapp_error_log = get_webcontainer_logs(wa_container)
+            if webapp_log is not None and webapp_error_log is not None:
                 log_files.append(webapp_log)
                 log_files.append(webapp_error_log)
         except Exception as e:
             print(e)
-            errors.append((container, e))
+            errors.append((wa_container.container, e))
 
     if errors:
         err_str = 'Container: {}\nException: {}'
@@ -87,6 +70,51 @@ def get_logs():
         raise exceptions.LogsNotCompleteError(exception_msg)
 
     return [lf for lf in log_files if lf is not None]
+
+
+def get_container_logs(wa_container):
+    container = wa_container.container
+    container_logs = docker_utils.get_container_logs(container)
+    log_filename = os.path.join(OUTPUT_FOLDER, '{}-container.log'.format(container.name))
+    docker_utils.write_to_file_in_binary(log_filename, container_logs)
+    return log_filename
+
+
+def get_container_inspect_logs(wa_container):
+    container = wa_container.container
+    inspect_log_filename = os.path.join(OUTPUT_FOLDER, '{}-inspect.log'.format(container.name))
+    inspect_result = docker_utils.get_inspect_result(container)
+    docker_utils.write_to_file(
+        inspect_log_filename,
+        json.dumps(inspect_result, indent=1),
+    )
+    return inspect_log_filename
+
+
+def get_corecontainer_coredumps_logs(wa_container):
+    container = wa_container.container
+    container_type = wa_container.container_type
+    core_dump_filename = None
+    if docker_utils.WA_COREAPP_CONTAINER_TAG == container_type:
+        core_dump_filename = os.path.join(OUTPUT_FOLDER, '{}-coredump.log'.format(container.name))
+        core_dump_results = docker_utils.get_core_dump_logs(container)
+        docker_utils.write_to_file(
+            core_dump_filename,
+            core_dump_results,
+        )
+    return core_dump_filename
+
+
+def get_webcontainer_logs(wa_container):
+    container = wa_container.container
+    container_type = wa_container.container_type
+    webapp_log_filename = None
+    webapp_error_log_filename = None
+    if docker_utils.WA_WEBAPP_CONTAINER_TAG == container_type:
+        webapp_log_filename = copy_additional_logs_for_webcontainer(container, WEB_LOG_PATH, WEB_LOG_FILE)
+        webapp_error_log_filename = \
+            copy_additional_logs_for_webcontainer(container, WEB_ERROR_LOG_PATH, WEB_ERROR_LOG_PATH)
+    return webapp_log_filename, webapp_error_log_filename
 
 
 def copy_additional_logs_for_webcontainer(container, path, file_name):
--- wadebug/wa_actions/test_log_utils.py
+++ wadebug/wa_actions/test_log_utils.py
@@ -6,6 +6,8 @@
 from wadebug import exceptions
 from wadebug.wa_actions import docker_utils
 from wadebug.wa_actions import log_utils
+from os import path
+import pytest
 
 
 def test_should_makedir_if_have_access(mocker):
@@ -17,35 +19,130 @@ def test_should_makedir_if_have_access(mocker):
     assert mocked_makedirs_call.call_count == 1
 
 
-def test_should_return_true_if_get_logs_has_no_exceptions(mocker):
+def test_should_return_filepath_string_if_get_container_logs_has_no_exceptions(mocker):
+    mock_container = docker_utils.WAContainer(MockContainer(), 'container_type')
+    expected_container_logs_filepath = 'filepath'
+    mocker.patch.object(
+        docker_utils, 'get_container_logs', return_value='This is a container log'
+    )
+    mocker.patch.object(
+        docker_utils, 'write_to_file_in_binary', return_value=None
+    )
+    mocker.patch.object(
+        path, 'join', return_value=expected_container_logs_filepath
+    )
+    try:
+        log_filepath = log_utils.get_container_logs(mock_container)
+    except Exception as e:
+        pytest.fail('Unexpected Exception from log_utils.get_container_logs: \n {}'.format(e))
+    assert log_filepath == expected_container_logs_filepath
+
+
+def test_should_return_filepath_string_if_get_container_inspect_logs_has_no_exceptions(mocker):
+    mock_container = docker_utils.WAContainer(MockContainer(), 'container_type')
+    mock_inspect_result = {'result': 'this is a mock result'}
+    expected_inspect_log_filepath = 'filepath'
+    mocker.patch.object(
+        docker_utils, 'get_inspect_result', return_value=mock_inspect_result
+    )
+    mocker.patch.object(
+        docker_utils, 'write_to_file', return_value=None
+    )
+    mocker.patch.object(
+        path, 'join', return_value=expected_inspect_log_filepath
+    )
+    try:
+        inspect_log_filepath = log_utils.get_container_inspect_logs(mock_container)
+    except Exception as e:
+        pytest.fail('Unexpected Exception from log_utils.get_container_inspect_logs: \n {}'.format(e))
+    assert inspect_log_filepath == expected_inspect_log_filepath
+
+
+def test_should_return_filepath_string_if_get_corecontainer_coredumps_logs_has_no_exceptions(mocker):
+    mock_container = docker_utils.WAContainer(MockContainer(), docker_utils.WA_COREAPP_CONTAINER_TAG)
+    expected_coredump_log_filepath = 'file_path'
+    mocker.patch.object(
+        docker_utils, 'get_core_dump_logs', return_value='Mock Coredump Result'
+    )
+    mocker.patch.object(
+        docker_utils, 'write_to_file', return_value=None
+    )
+    mocker.patch.object(
+        path, 'join', return_value=expected_coredump_log_filepath
+    )
+    try:
+        coredump_log_filepath = log_utils.get_corecontainer_coredumps_logs(mock_container)
+    except Exception as e:
+        pytest.fail('Unexpected Exception from log_utils.get_corecontainer_coredumps_logs: \n {}'.format(e))
+    assert coredump_log_filepath == expected_coredump_log_filepath
+
+
+def test_should_return_none_if_get_corecontainer_coredumps_logs_on_not_core_container(mocker):
+    mocker_container = docker_utils.WAContainer(MockContainer(), 'not core container type')
+    try:
+        result = log_utils.get_corecontainer_coredumps_logs(mocker_container)
+    # there should be no exception, since get_corecontainer_coredumps
+    # should not run any code to pull logs if the container is not the core container
+    except Exception as e:
+        pytest.fail('Unexpected Exception from log_utils.get_corecontainer_coredumps_logs: \n {}'.format(e))
+    assert result is None
+
+
+def test_should_return_filepath_string_if_get_webcontainer_logs_has_no_exceptions(mocker):
+    mock_container = docker_utils.WAContainer(MockContainer(), docker_utils.WA_WEBAPP_CONTAINER_TAG)
+    expected_log_filepath = '/logs'
+    mocker.patch.object(
+        log_utils, 'copy_additional_logs_for_webcontainer', return_value=expected_log_filepath
+    )
+    try:
+        webapp_log_filepath, webapp_error_log_filepath = log_utils.get_webcontainer_logs(mock_container)
+    except Exception as e:
+        pytest.fail('Unexpected Exception from log_utils.get_webcontainer_logs:\n{}'.format(e))
+    assert webapp_log_filepath == expected_log_filepath and webapp_error_log_filepath == expected_log_filepath
+
+
+def test_should_return_none_if_get_webcontainer_logs_on_not_web_container(mocker):
+    mocker_container = docker_utils.WAContainer(MockContainer(), 'not web container type')
+    try:
+        log_result, error_result = log_utils.get_webcontainer_logs(mocker_container)
+    # there should be no exception, since get_webcontainer_logs
+    # should not run any code to pull logs if the container is not the web container
+    except Exception as e:
+        pytest.fail('Unexpected Exception from log_utils.get_webcontainer_logs:\n{}'.format(e))
+    assert log_result is None and error_result is None
+
+
+def test_should_return_list_of_filepaths_for_logs_if_get_logs_has_no_exceptions(mocker):
     mock_containers = [
-        docker_utils.WAContainer(MockContainer(), 'container_type'),
         docker_utils.WAContainer(MockContainer(), 'container_type'),
     ]
     mocker.patch.object(
         docker_utils, 'get_wa_containers', return_value=mock_containers
     )
     mocker.patch.object(
-        docker_utils, 'get_container_logs', return_value='Fake container logs'
+        log_utils, 'get_container_logs', return_value='/container/logs'
     )
     mocker.patch.object(
-        docker_utils, 'get_inspect_result', return_value='Fake inspect results'
+        log_utils, 'get_container_inspect_logs', return_value='/inspect/logs'
     )
     mocker.patch.object(
-        docker_utils, 'get_core_dump_logs', return_value='Fake Core Dumps'
+        log_utils, 'get_corecontainer_coredumps_logs', return_value='/core_dump/logs'
     )
     mocker.patch.object(
-        docker_utils, 'write_to_file_in_binary', return_value=None
+        log_utils, 'get_webcontainer_logs', return_value=('/webcontainer/logs', '/webcontainer/errors')
     )
-    mocker.patch.object(
-        docker_utils, 'write_to_file', return_value=None
-    )
-
+    expected_logs_files = [
+        '/container/logs',
+        '/inspect/logs',
+        '/core_dump/logs',
+        '/webcontainer/logs',
+        '/webcontainer/errors'
+    ]
     try:
-        log_utils.get_logs()
-        assert True
+        results = log_utils.get_logs()
     except exceptions.LogsNotCompleteError:
-        assert False
+        pytest.fail("Access error:  Cannot write logs to current directory")
+    assert results == expected_logs_files
 
 
 def test_should_write_webcontainer_log_to_file(mocker):
