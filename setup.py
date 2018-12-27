f4ec1e72af17aed84809c6443853e97602c11c71
Alan Wong <alanwong@fb.com>
2018-12-26 23:52:27 -0800

    version bump for new release
    Summary: bumping version for new release
    
    Reviewed By: mengyiyuan
    
    Differential Revision: D13552231
    
    fbshipit-source-id: d7e3c7ab

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     name='wadebug',
     long_description=open('README.md', 'r').read(),
     long_description_content_type='text/markdown',
-    version='0.1.1',
+    version='0.1.2',
     url='https://developers.facebook.com/docs/whatsapp/guides/wadebug',
     license='MIT',
     author='Thiago Moraes',
